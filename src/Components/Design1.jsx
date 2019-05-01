419b27d389102a9587a50f6bdb658fada9d14a10
kyblockstacking <kevin1992yang@gmail.com>
2019-05-01 01:48:43 -0700

    initial design1

---
+++ src/Components/Design1.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import '../Stylesheets/Design1.css';
+
+export default class Design1 extends Component {
+    render() {
+        return (
+            <div>design 1</div>
+        );
+    };
+};
