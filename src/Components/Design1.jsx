9cf9a542900d6519152323eacfaf516373bf9cb1
kyblockstacking <kevin1992yang@gmail.com>
2019-05-01 03:57:37 -0700

    design1 work

--- src/Components/Design1.jsx
+++ src/Components/Design1.jsx
@@ -4,7 +4,21 @@ import '../Stylesheets/Design1.css';
 export default class Design1 extends Component {
     render() {
         return (
-            <div>design 1</div>
+            <div className='wrapper-design-1'>
+
+                <form class='form-design-1' action="" method="post">
+                    <label for="username">
+                        <span>Username</span><input className='input-username' type="text" name="username" required="true" />
+                    </label>
+                    <label for="password">
+                        <span>Password</span><input className='input-password' type="password" name="password" required="true" />
+                    </label>
+                    <label>
+                        <span> </span><input type="submit" value="Sign In" />
+                    </label>
+                </form>
+
+            </div>
         );
     };
 };
