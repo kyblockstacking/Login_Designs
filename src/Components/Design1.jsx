5792d40b1e445b450f2bd5baed4a34d40f340a93
kyblockstacking <kevin1992yang@gmail.com>
2019-05-06 04:14:55 -0700

    some fixes

--- src/Components/Design1.jsx
+++ src/Components/Design1.jsx
@@ -6,25 +6,26 @@ export default class Design1 extends Component {
         return (
             <div className='wrapper-design-1'>
 
-                <form class='form-design-1' action=''>
-                    <label for='username'>
+                <form className='form-design-1' action=''>
+                    <label htmlFor='username'>
                         <span className='text-username'>Username</span>
-                        <input className='input-username' type='text' name='username' required='true' />
+                        <input className='input-username' type='text' name='username' required={true} />
                     </label>
-                    <label for='password'>
+                    <label htmlFor='password'>
                         <span className='text-password'>Password</span>
-                        <input className='input-password' type='password' name='password' required='true' />
+                        <input className='input-password' type='password' name='password' required={true} />
                     </label>
                     <label>
                         <input className='sign-in' type='submit' value='Sign In' />
                     </label>
-                    <label class='kevin'>
+                    <label className='kevin'>
                         <input type='checkbox' />
-                        <span class='checkmark'></span>Stay signed in
+                        <span className='checkmark'></span>Stay signed in
                     </label>
                 </form>
 
             </div>
         );
     };
+
 };
